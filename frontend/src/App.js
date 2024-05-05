7afc13a65aa86db822ad2d934160fb5ba14a805f
annaanitha1998 <annaanitha.09@gmail.com>
2024-05-05 12:54:32 +0400

    fixed login logout issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,13 @@ import Cookies from "universal-cookie";
 import Register from "./component/Register";
 import Login from "./component/Login";
 import Home from "./component/Home";
+import { useEffect, useState } from "react";
 
 function App() {
   const cookies = new Cookies();
+
+  const [authenticationString, setAuthenticationString] = useState("");
   const token = cookies.get("TOKEN");
-  console.log(token)
 
   const navigation = [
     {
@@ -28,7 +30,11 @@ function App() {
     }
   ];
 
-  const handleLoginOrLogout = () => {
+  useEffect(() => {
+    if(token) setAuthenticationString('Logout')
+    else setAuthenticationString('Login')
+  })
+  const handleauthenticationString = () => {
     if (!token) {
       window.location.href = "/login";
     } else {
@@ -51,8 +57,8 @@ function App() {
           ))}
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-lg font-semibold leading-6 text-white" onClick={() => handleLoginOrLogout()}>
-            {{ token } ? 'Login' : 'Log out'} <span aria-hidden="true">&rarr;</span>
+          <a href="#" className="text-lg font-semibold leading-6 text-white" onClick={() => handleauthenticationString()}>
+            {authenticationString} <span aria-hidden="true">&rarr;</span>
           </a>
         </div>
       </nav>
